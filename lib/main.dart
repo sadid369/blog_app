d80678bbf059a8d616f35c774112178ab9711647
sadid369 <sadid.jones@gmail.com>
2024-04-07 14:15:20 +0600

    blog app finished @ 2:38

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,17 @@
-import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
-import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentaion/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentaion/pages/login_page.dart';
-import 'package:blog_app/features/auth/presentaion/pages/signup_page.dart';
+import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-  );
-
+  await initDependencies();
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
-        create: (context) => AuthBloc(
-          userSignUp: UserSignUp(
-            AuthRepositoryImpl(
-              AuthRemoteDataSourceImpl(
-                supabase.client,
-              ),
-            ),
-          ),
-        ),
+        create: (context) => serviceLocator<AuthBloc>(),
       )
     ],
     child: const MyApp(),
