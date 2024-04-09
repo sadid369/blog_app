451b6c9e55ae084fcf2131ad82889dce3bc45626
sadid369 <sadid.jones@gmail.com>
2024-04-10 01:19:56 +0600

    blog app finished @ 3:14

--- lib/features/auth/presentaion/pages/login_page.dart
+++ lib/features/auth/presentaion/pages/login_page.dart
@@ -1,9 +1,13 @@
+import 'package:blog_app/core/common/widgets/loader.dart';
 import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/utils/show_snackbar.dart';
+import 'package:blog_app/features/auth/presentaion/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentaion/pages/signup_page.dart';
 import 'package:blog_app/features/auth/presentaion/widgets/auth_field.dart';
 import 'package:blog_app/features/auth/presentaion/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginPage extends StatefulWidget {
   static route() => MaterialPageRoute(
@@ -33,70 +37,93 @@ class _LoginPageState extends State<LoginPage> {
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Center(
-          child: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Sign In.',
-                    style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  AuthField(
-                    hintText: 'Email',
-                    controller: emailController,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  AuthField(
-                    hintText: 'Password',
-                    controller: passwordController,
-                    isObscureText: true,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  AuthGradientButton(
-                    onPressed: () {},
-                    buttonText: 'Sign In',
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(LoginPage.route());
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Don't have an account?  ",
-                          style: Theme.of(context).textTheme.titleMedium,
+          child: BlocConsumer<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is AuthFailure) {
+                showSnackBar(context, state.message);
+              }
+            },
+            builder: (context, state) {
+              if (state is AuthLoading) {
+                return const Loader();
+              }
+              return Form(
+                key: formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Sign In.',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          'Sign Up',
-                          style:
-                              Theme.of(context).textTheme.titleMedium?.copyWith(
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      AuthField(
+                        hintText: 'Email',
+                        controller: emailController,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      AuthField(
+                        hintText: 'Password',
+                        controller: passwordController,
+                        isObscureText: true,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      AuthGradientButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            context.read<AuthBloc>().add(
+                                  AuthLogin(
+                                    email: emailController.text.trim(),
+                                    password: passwordController.text.trim(),
+                                  ),
+                                );
+                          }
+                        },
+                        buttonText: 'Sign In',
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(LoginPage.route());
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Don't have an account?  ",
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            Text(
+                              'Sign Up',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium
+                                  ?.copyWith(
                                     color: AppPallete.gradient2,
                                     fontWeight: FontWeight.bold,
                                   ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
